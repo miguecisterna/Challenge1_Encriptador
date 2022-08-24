26a49a89b1172cf14b8112243a31bf3bd7584765
Migue C <miguecisterna@gmail.com>
2022-08-24 00:59:41 -0300

    Retocados los bordes del text Area para que no aparezan /Modificada la funcion del botón copiar /Añadidas imagenes hechas por mí

--- script.js
+++ script.js
@@ -9,6 +9,15 @@ function btnEncriptar(){
     inputTexto.value = "";
 }
 
+function btnDesencriptar(){
+
+    const textoDesenctiptado = desencriptar(inputTexto.value);
+    mensaje.value = textoDesenctiptado;
+    mensaje.style.background = "#E09FA8";
+    inputTexto.value = "";
+}
+
+
 function encriptar(stringEncriptado){
 
     let matriz = [["e","enter"], ["i","imes"],["a","ai"],["o","ober"],["u","ufat"]];
@@ -16,10 +25,33 @@ function encriptar(stringEncriptado){
 
     for(let i = 0; i < matriz.length; i++){
         if(stringEncriptado.includes(matriz[i][0])){
-            stringEncriptado = stringEncriptado.replaceAll(matriz[i][0],matriz[i][1])
+            stringEncriptado = stringEncriptado.replaceAll(matriz[i][0],matriz[i][1]);
         }
     }
 
     return stringEncriptado;
 
+}
+
+function desencriptar(stringDesencriptado){
+
+    let matriz = [["e","enter"],["i","imes"],["a","ai"],["o","ober"],["u","ufat"]];
+    stringDesencriptado = stringDesencriptado.toLowerCase();
+
+    for(let i = 0; i < matriz.length; i++){
+        if(stringDesencriptado.includes(matriz[i][1])){
+            stringDesencriptado =stringDesencriptado.replaceAll(matriz[i][1],matriz[i][0]);
+        }
+    }
+
+    return stringDesencriptado;
+}
+
+function copiar(){
+    mensaje.select();
+    navigator.clipboard.writeText(mensaje.value);
+    mensaje.value = "";
+    mensaje.style.background = "#E09FA8;";
+    mensaje.style.backgroundImage = "url('images/lock.png')";
+    mensaje.style.backgroundSize = "100%";
 }
